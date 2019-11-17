fe5a6729f15a3b05169c548c7153829debf1e959
Karen Pilosyan <pilosyan.karen@gmail.com>
2019-11-18 03:21:12 +0400

    update

---
+++ week1/homework2.py
@@ -0,0 +1,18 @@
+project = 'cake'
+difficulty = 5
+ingredients = ["flour", "butter", "sugar", "eggs", "cocoa powder", "baking powder"]
+
+print('apples' in ingredients)
+print('butter' in ingredients)
+print(('eggs' in ingredients) or ('margarine' in ingredients))
+print(('eggs' in ingredients) and ('margarine' in ingredients))
+
+
+project = 'cake'
+difficulty = 5
+ingredients = ["flour", "butter", "sugar", "eggs", "cocoa powder", "baking powder"]
+
+print('apples' in ingredients)
+print('butter' in ingredients)
+print(('eggs' in ingredients) or ('margarine' in ingredients))
+print(('eggs' in ingredients) and ('margarine' in ingredients))
