b939f6ae91c85fc7dc162e6e9b66d6642baaf03f
Karen Pilosyan <pilosyan.karen@gmail.com>
2019-11-26 00:00:39 +0400

    Updates

--- week5/Customer.py
+++ week5/Customer.py
@@ -1,2 +1,10 @@
-products = {"candy": 10, "juice": 5, "pen": 50}
+import Productcheck
 
+def buy(product, num, price):
+    if Productcheck.check(product, num) == True:
+        print("You bought", product, "and spent", str(num*price))
+    else:
+        print("Sorry! We are out of this product")
+
+
+buy("candy", 100, 100)
